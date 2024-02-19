0991b4208b8fe1d1373af5a545d706a48bed1c89
CarmenAmez <carmensasamez@gmail.com>
2024-02-19 19:54:29 +0100

    ejercicios 19/02/2024

---
+++ CursoJava/inicios js/rqr.js
@@ -0,0 +1,30 @@
+console.log('Hola!')
+
+let number=7;
+console.log(number)
+
+let texto='habitacisón';
+console.log(texto)
+
+let m;
+m = 7;
+let h = m;
+console.log(h);
+
+let tituloDeLaPelicula;
+let camelCaseBienPuesto;
+let edadDelMonstruo;
+
+let i = 7;
+i = 5;
+i = 3;
+console.log(i);
+//
+
+let a = 5;
+let b = 7;
+let c = b;
+b=a;
+a=c;
+console.log('a: ', a); //Debería mostrar 7
+console.log('b: ', b); //Debería mostrar 5
