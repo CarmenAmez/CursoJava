d674e78605d19fc590516d05b230f88f49d6c198
CarmenAmez <carmensasamez@gmail.com>
2024-03-11 15:11:23 +0100

    ejercicios fin de semana

--- CursoJava/inicios js/tresraya.js
+++ CursoJava/inicios js/tresraya.js
@@ -1,43 +1,84 @@
 const board = ['_', '_', '_', '_', '_', '_', '_', '_', '_'];
 
+// Funcion para mostrar el tablero
 const printBoard = () => {
     alert(board[0] + "|" + board[1] + "|" + board[2] + "\n" + board[3] + "|" + board[4] + "|" + board[5] + "\n" + board[6] + "|" + board[7] + "|" + board[8]);
 }
-
-const evaluateWin = () => {
-    if (board[0] === board[1] && board[0] === board[2] && board[0] != '_') {
-        return true;
-    } else if (board[3] === board[4] && board[3] === board[5] && board[3] !== '_') {
-        return true;
-    } else if (board[6] === board[7] && board[7] === board[8] && board[6] !== '_') {
-        return true;
-    } else if (board[0] === board[3] && board[0] === board[6] && board[0] !== '_') {
-        return true;
-    } else if (board[1] === board[4] && board[1] === board[7] && board[1] !== '_') {
-        return true;
-    } else if (board[2] === board[5] && board[2] === board[8] && board[2] !== '_') {
-        return true;
-    } else if (board[0] === board[4] && board[0] === board[8] && board[0] !== '_') {
-        return true;
-    } else if (board[2] === board[4] && board[2] === board[7] && board[2] !== '_') {
-        return true;
+//Funcion para comprobar si el jugador ha ganado.
+const checkIfPlayerWon = (board) => {
+    if (board[0] === board[2] && board[1] === board[2] && board[2] !== '_') {
+        return true
+    } else if (board[3] === board[5] && board[4] === board[5] && board[5] !== '_') {
+        return true
+    } else if (board[6] === board[8] && board[7] === board[8] && board[8] !== '_') {
+        return true
+    } else if (board[0] === board[6] && board[3] === board[6] && board[6] !== '_') {
+        return true
+    } else if (board[1] === board[7] && board[4] === board[7] && board[7] !== '_') {
+        return true
+    } else if (board[2] === board[8] && board[5] === board[8] && board[8] !== '_') {
+        return true
+    } else if (board[0] === board[8] && board[4] === board[8] && board[8] !== '_') {
+        return true
+    } else if (board[2] === board[6] && board[4] === board[6] && board[6] !== '_') {
+        return true
+    } else {
+        return false
     }
-    return false;
 }
-printBoard();
+
+const checkIfTable = (board) => {
+    if (
+        board[0] !== '_' &&
+        board[1] !== '_' &&
+        board[2] !== '_' &&
+        board[3] !== '_' &&
+        board[4] !== '_' &&
+        board[5] !== '_' &&
+        board[6] !== '_' &&
+        board[7] !== '_' &&
+        board[8] !== '_'
+    ) {
+        return true
+    } else {
+        return false
+    }
+}
+
+//Funcion para colocar la pieza
+const setPiece = (board, piece) => {
+    let checkPosition = false
+    //Bucle para comprobar que la posición está libre.
+    while (!checkPosition) {
+        let position = prompt('Introduce un número de 1 al 9: ') - 1;
+        if (board[position] === '_') {
+            board[position] = piece;
+            checkPosition = true
+        } else {
+            alert('Posición ocupada! Escoja otro número!')
+        }
+    }
+}
+//Instancio las variables del juego 
 let isPlayer1 = true;
-let hasAnyBodyWon = false;
+let gameOver = false
 
-while (!hasAnyBodyWon) {
-    const player = isPlayer1 ? 'X' : 'O';
-    const n = prompt('Introduzca un número del 0 al 8, player ' + player + ':');
-    board[n] = player;
+printBoard();
 
-    if (evaluateWin()) {
-        alert('Enhorabuena player ' + player);
-        hasAnyBodyWon = true;
+//Lógica del juego.
+while (!gameOver) {
+    let player = isPlayer1 ? 'X' : 'O';
+    alert('Turno del jugador ' + player)
+    setPiece(board, player);
+    printBoard();
+    gameOver = checkIfPlayerWon(board);
+    if (gameOver) {
+        alert('Ha ganado el Jugador ' + player + ' ¡Enhorabuena!')
     } else {
         isPlayer1 = !isPlayer1;
-        printBoard();
+        gameOver = checkIfTable(board)
+        if (gameOver) {
+            alert('El juego acaba en Empate!')
+        }
     }
 }
